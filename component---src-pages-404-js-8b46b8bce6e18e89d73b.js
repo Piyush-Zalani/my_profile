6d0d932787d3998959cb4888babffc796f80c11a
Piyush Zalani <shanky.q3@gmail.com>
2020-10-06 20:10:51 +0530

    Updates

--- component---src-pages-404-js-8b46b8bce6e18e89d73b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(n,o,t){"use strict";t.r(o),t.d(o,"default",(function(){return w}));t("q1tI");function w(){return"undefined"!=typeof window&&(window.location="/"),null}}}]);
-//# sourceMappingURL=component---src-pages-404-js-8b46b8bce6e18e89d73b.js.map
