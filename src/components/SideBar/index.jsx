ff3b625d74a2d1b97e1dee71478fa8a1e9229cbc
Piyush Zalani <pzalani@paloaltonetworks.com>
2022-01-10 04:36:20 +0530

    Update index.jsx
    Modified drive url

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -37,7 +37,7 @@ class SideBar extends React.PureComponent {
                 return (
                   <StyledLI key={item.id}>
                     <StyledLink
-                      href="https://drive.google.com/file/d/1fGhUUp9A1NL7tW3pS_hbLOD4Bxx9ML7J/view?usp=sharing"
+                      href="https://docs.google.com/document/d/1Qjwhh3oQ82kAW4s4cW9AOA8fBNDv9t19/edit?usp=sharing&ouid=100755501973233206770&rtpof=true&sd=true"
                       target="_blank"
                       color={item.color}
                     >
